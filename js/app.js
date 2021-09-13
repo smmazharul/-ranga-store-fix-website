d5609dd0ffb27c1dbcd57edfea72c780c7ca4ad6
smmazharul <smmazharulislam20@gmail.com>
2021-09-13 18:10:03 +0600

    rating

--- js/app.js
+++ js/app.js
@@ -18,8 +18,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <p>Ratings: ${product.rating.rate}</p>
-      <p>Total Ratings: ${product.rating.count}</p>
+      <p>Ratings: <span class="rating-rate">${product.rating.rate}</span></p>
+      <p>Total Ratings: <span class="rating-count">${product.rating.count}</span></p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button id="details-btn" class="btn btn-danger">Details</button></div>
